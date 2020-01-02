9beb3ddb64038f79f84e6c4ce3a5240bf432acc0
Akhaphon <605021000787@mail.rmutk.ac.th>
2020-01-02 11:49:16 +0700

    finish

--- DB/fromDB_get.php
+++ DB/fromDB_get.php
@@ -13,9 +13,16 @@
     }
 
     // // prepare and bind
-    // $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES(?,?,?)");
-    // $stmt-> bind_param("sss", $firstname, $lastname, $email);
+    $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES(?,?,?)");
+    $stmt-> bind_param("sss", $firstname, $lastname, $email);
 
-    var_dump($_post);
-    
+    // var_dump($_POST);
+    $firstname = $_POST["firstname"];
+    $lastname = $_POST["lastname"];
+    $email = $_POST["email"];
+    $stmt-> execute();
+    echo "New Records create successfully.";
+    $stmt->close();
+    $conn->close();
+
 ?>
